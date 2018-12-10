4f877da71dce4f7b789199835cde4aa9914fba23
CJ Grady <cjgrady@gmail.com>
2018-12-10 11:00:35 -0500

    Updated setup and added an init file

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
     name='ot_service_wrapper',
     version='0.1',
-    packages=find_packages(exclude=['tests*']),
-    license='gpl3',
-    description='Python wrapper for Open Tree services',
-    author='CJ Grady',
-    author_email='cjgrady@ku.edu'
+    description='Biotaphy package for wrapping Open Tree services',
+    long_description=readme,
+    author='Biotaphy Team',
+    author_email='cjgrady@ku.edu',
+    url='https://github.com/biotaphy/ot_service_wrapper',
+    license=license,
+    packages=find_packages(exclude=('tests')),
+    install_requires=[
+        'dendropy>=4.0.0'
+    ]
 )
+
